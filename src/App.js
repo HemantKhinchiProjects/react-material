b2d2999cd9e7c687900061ab1624f8d7bde8caa5
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-12 17:41:20 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "./style.css";
 export default function App() {
   return (
     <div className="MuiBox-root css-9jay18">
-      <div class="MuiPaper-root MuiPaper-outlined MuiPaper-rounded css-ico5vo">
+      <div className="MuiPaper-root MuiPaper-outlined MuiPaper-rounded css-ico5vo">
       <div>
-        <span class="MuiBadge-root css-1rzb3uu">
-        <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall css-1k33q06" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ShoppingCartRoundedIcon"><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 3c0 .55.45 1 1 1h1l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h11c.55 0 1-.45 1-1s-.45-1-1-1H7l1.1-2h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.37-.66-.11-1.48-.87-1.48H5.21l-.67-1.43c-.16-.35-.52-.57-.9-.57H2c-.55 0-1 .45-1 1zm16 15c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"></path></svg>
+        <span className="MuiBadge-root css-1rzb3uu">
+        <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall css-1k33q06" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ShoppingCartRoundedIcon"><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 3c0 .55.45 1 1 1h1l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h11c.55 0 1-.45 1-1s-.45-1-1-1H7l1.1-2h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.37-.66-.11-1.48-.87-1.48H5.21l-.67-1.43c-.16-.35-.52-.57-.9-.57H2c-.55 0-1 .45-1 1zm16 15c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"></path></svg>
         </span>
       </div>
       </div>
